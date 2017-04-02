88480b2ad03b06ebee54349ff6a5153c23045444
ryanSn <ryan.chatterton@sharpnotions.com>
2017-04-02 08:06:39 -0400

    feat: add tray icon with open and close

---
+++ tray.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const electron = require('electron');
+
+const app = electron.app
+let tray = null
+
+exports.create = win => {
+  if (tray) {
+    return
+  }
+
+  const iconPath = path.join(__dirname, 'assets/icons/win/icon.ico')
+
+  const toggleWindow = () => {
+    if (win.isVisible()) {
+      win.hide()
+    } else {
+      win.show()
+    }
+  }
+
+  const contextMenu = electron.Menu.buildFromTemplate([
+    {
+      label: 'Toggle',
+      click() {
+        toggleWindow()
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'quit'
+    }
+  ])
+
+ tray = new electron.Tray(iconPath)
+ tray.setToolTip(`${app.getName()}`)
+ tray.setContextMenu(contextMenu)
+ tray.on('click', toggleWindow)
+}
