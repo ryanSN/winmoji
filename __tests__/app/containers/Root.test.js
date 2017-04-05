8f22d0be7657720091781f4c0531f67e665d9dd4
ryanSn <ryan.chatterton@sharpnotions.com>
2017-04-04 23:29:56 -0400

    chore: Simple Jest tests

---
+++ __tests__/app/containers/Root.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Root from '../../../app/containers/Root'
+
+import renderer from 'react-test-renderer'
+
+describe('app', () => {
+  it('should render the base app', () => {
+    const componet = renderer.create(<Root />)
+    const json = componet.toJSON()
+    expect(json).toMatchSnapshot()
+  })
+})
