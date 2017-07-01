6e171332799a354d595daca081233380aaadb65b
ryanSn <ryan.chatterton@sharpnotions.com>
2017-07-01 18:33:02 -0400

    chore: add mocks for electron

---
+++ __mocks__/electron.js
@@ -0,0 +1,8 @@
+module.exports = {
+  require: jest.genMockFunction(),
+  match: jest.genMockFunction(),
+  app: jest.genMockFunction(),
+  remote: jest.genMockFunction(),
+  dialog: jest.genMockFunction(),
+  ipcRenderer: { on : jest.fn ()}
+}
