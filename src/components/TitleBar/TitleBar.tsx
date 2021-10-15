bccb34afb18972aaef1d7596f5c050e7021aacd2
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2021-10-15 15:30:29 -0400

    feat: titlebar changes (#209)

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Json from '../../../package.json';
 import { Props } from './TitleBar.types';
+import {
+  StyledAppName,
+  StyledCloseButton,
+  StyledLogo,
+  StyledMinimizeButton,
+  StyledTitleBar,
+  StyledWindowActions,
+  StyledWrapper,
+} from './TitleBarStyles';
+
+import appIcon from '../../assets/icons/16x16.png';
+import { ElectronContext } from '../../contexts';
 
 const TitleBar = ({ darkMode }: Props) => {
+  const { onMinimizeCallback, onExitCallback } = useContext(ElectronContext);
+
+  const onMin = () => {
+    if (onMinimizeCallback) {
+      onMinimizeCallback();
+    }
+  };
+
   return (
-    <div>
-      {Json.productName} {Json.version && <span>{Json.version}</span>}
-      Darkmode: {darkMode}
-    </div>
+    <StyledTitleBar>
+      <StyledWrapper>
+        <StyledLogo src={appIcon} />
+        <StyledAppName>
+          {Json.productName} {Json.version && <span>{Json.version}</span>}
+        </StyledAppName>
+      </StyledWrapper>
+      <StyledWindowActions>
+        <StyledMinimizeButton onClick={onMin} />
+        <StyledCloseButton onClick={onExitCallback} />
+      </StyledWindowActions>
+    </StyledTitleBar>
   );
 };
 
--- src/contexts/ElectronContext.tsx
+++ src/contexts/ElectronContext.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { CLIPBOARD_WRITE } from '../shared';
+import React, { useCallback } from 'react';
+import { CLIPBOARD_WRITE, SET_CLOSE, SET_MINIMIZE } from '../shared';
 type ElectronProps = {
   onClipboardWrite?: (char: string | undefined) => void;
-  sendPing?: () => void;
+  onMinimizeCallback?: () => void;
+  onExitCallback?: () => void;
 };
 
 type ElectronProviderProps = {
   children?: React.ReactNode;
+  onMinimizeCallback?: () => void;
+  onExitCallback?: () => void;
 };
 
 const ElectronContext = React.createContext<ElectronProps>({});
@@ -19,8 +22,20 @@ const ElectronProvider = ({ children }: ElectronProviderProps) => {
     electron.send(CLIPBOARD_WRITE, char);
   };
 
+  const onMinimizeCallback = () => {
+    electron.send(SET_MINIMIZE, {});
+  };
+
+  const onExitCallback = useCallback(() => {
+    electron.send(SET_CLOSE, {
+      closeToTray: true,
+    });
+  }, [electron]);
+
   return (
-    <ElectronContext.Provider value={{ onClipboardWrite }}>{children}</ElectronContext.Provider>
+    <ElectronContext.Provider value={{ onClipboardWrite, onExitCallback, onMinimizeCallback }}>
+      {children}
+    </ElectronContext.Provider>
   );
 };
 
